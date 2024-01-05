53d8b0a8476288e4cd4c664000380e84dd411b95
Rayen Hamada <rayeeene@gmail.com>
2024-01-05 22:01:57 +0100

    fix mail sending in payment flow

--- config/multerConfig.js
+++ config/multerConfig.js
@@ -1,104 +1,112 @@
-const multer = require('multer');
-const path = require('path');
-const { S3Client } = require('@aws-sdk/client-s3');
-const multerS3 = require('multer-s3');
-const crypto = require('crypto');
-
-
+const multer = require("multer");
+const path = require("path");
+const { S3Client } = require("@aws-sdk/client-s3");
+const multerS3 = require("multer-s3");
 
 const s3 = new S3Client({
-  accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
+  region: process.env.AWS_S3_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
+  },
 });
-
-
 
 const metadata = {};
 
 exports.fileCloudUpload = multer({
   storage: multerS3({
     s3: s3,
-    acl: 'public-read',
+    acl: "public-read",
     contentType: multerS3.AUTO_CONTENT_TYPE,
     bucket: process.env.AWS_S3_TEXTURE_BUCKET,
     metadata: function (req, file, cb) {
       cb(null, {
-        fieldName: file.fieldname + '_' + Date.now() + path.extname(file.originalname),
-        ...metadata
+        fieldName:
+          file.fieldname + "_" + Date.now() + path.extname(file.originalname),
+        ...metadata,
       });
     },
     key: function (req, file, cb) {
-      cb(null, file.fieldname + '_' + Date.now()
-        + path.extname(file.originalname))
-    }
-  })
+      cb(
+        null,
+        file.fieldname + "_" + Date.now() + path.extname(file.originalname)
+      );
+    },
+  }),
 });
-
 
 exports.logoCloudUpload = multer({
   storage: multerS3({
     s3: s3,
-    acl: 'public-read',
+    acl: "public-read",
     contentType: multerS3.AUTO_CONTENT_TYPE,
     bucket: process.env.AWS_S3_TEXTURE_BUCKET,
     metadata: function (req, file, cb) {
       cb(null, { fieldName: file.fieldname });
     },
     key: function (req, file, cb) {
-      cb(null, file.fieldname + '_' + Date.now()
-        + path.extname(file.originalname))
-    }
-  })
-})
+      cb(
+        null,
+        file.fieldname + "_" + Date.now() + path.extname(file.originalname)
+      );
+    },
+  }),
+});
 
 exports.videoCloudUpload = multer({
   storage: multerS3({
     s3: s3,
-    acl: 'public-read',
+    acl: "public-read",
     contentType: multerS3.AUTO_CONTENT_TYPE,
     bucket: process.env.AWS_S3_TEXTURE_BUCKET,
     metadata: function (req, file, cb) {
       cb(null, { fieldName: file.fieldname });
     },
     key: function (req, file, cb) {
-      cb(null, file.fieldname + '_' + Date.now()
-        + path.extname(file.originalname))
-    }
-  })
-})
+      cb(
+        null,
+        file.fieldname + "_" + Date.now() + path.extname(file.originalname)
+      );
+    },
+  }),
+});
 
 const imageStorage = multer.diskStorage({
-  // Destination to store image     
-  destination: 'public',
+  // Destination to store image
+  destination: "public",
   filename: (req, file, cb) => {
-    cb(null, file.fieldname + '_' + Date.now()
-      + path.extname(file.originalname))
+    cb(
+      null,
+      file.fieldname + "_" + Date.now() + path.extname(file.originalname)
+    );
     // file.fieldname is name of the field (image)
     // path.extname get the uploaded file extension
-  }
+  },
 });
 
 exports.imageUpload = multer({
   storage: imageStorage,
   limits: {
-    fileSize: 10000000 // 1000000 Bytes = 1 MB
-  }
-})
+    fileSize: 10000000, // 1000000 Bytes = 1 MB
+  },
+});
 
 const pdfStorage = multer.diskStorage({
-  // Destination to store image     
-  destination: 'public',
+  // Destination to store image
+  destination: "public",
   filename: (req, file, cb) => {
-    cb(null, file.fieldname + '_' + Date.now()
-      + path.extname(file.originalname))
+    cb(
+      null,
+      file.fieldname + "_" + Date.now() + path.extname(file.originalname)
+    );
     // file.fieldname is name of the field (image)
     // path.extname get the uploaded file extension
-  }
+  },
 });
 
 exports.pdfUpload = multer({
   storage: pdfStorage,
   limits: {
-    fileSize: 100000000 // 1000000 Bytes = 1 MB
-  }
-})
+    fileSize: 100000000, // 1000000 Bytes = 1 MB
+  },
+});
--- controllers/stripeController.js
+++ controllers/stripeController.js
@@ -1,18 +1,17 @@
-
-const mongoose = require('mongoose'),
-  Visitor = mongoose.model('Visitors'),
-  Exhibition = mongoose.model('Exhibitions'),
+const mongoose = require("mongoose"),
+  Visitor = mongoose.model("Visitors"),
+  Exhibition = mongoose.model("Exhibitions"),
   Ticket = mongoose.model("Tickets"),
-  nodemailer = require('nodemailer'),
-  fs = require('fs');
-stripe = require('stripe')(process.env.STRIPE_SECRET_KEY),
-
-  exports.webhook = async (req, res) => {
+  nodemailer = require("nodemailer"),
+  { sendEmail } = require("../helpers/email"),
+  fs = require("fs");
+(stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)),
+  (exports.webhook = async (req, res) => {
     let event;
     try {
       event = stripe.webhooks.constructEvent(
         req.body,
-        req.header('Stripe-Signature'),
+        req.header("Stripe-Signature"),
         process.env.STRIPE_WEBHOOK_SECRET
       );
     } catch (err) {
@@ -32,12 +31,12 @@ stripe = require('stripe')(process.env.STRIPE_SECRET_KEY),
     console.log(dataObject);
     console.log(event.type);
     switch (event.type) {
-      case 'payment_intent.succeeded':
-        if (dataObject['metadata']['email']) {
-          let metadata = dataObject['metadata'];
+      case "payment_intent.succeeded":
+        if (dataObject["metadata"]["email"]) {
+          let metadata = dataObject["metadata"];
           var password;
           password = Math.random().toString(36).slice(-8);
-          Visitor.findOne({ 'email': metadata.email }, (err, visitor) => {
+          Visitor.findOne({ email: metadata.email }, (err, visitor) => {
             if (!err) {
               if (visitor) {
                 var ticket = new Ticket();
@@ -47,64 +46,83 @@ stripe = require('stripe')(process.env.STRIPE_SECRET_KEY),
                 ticket.save((err, ticketDoc) => {
                   if (err) {
                     res.status(400).send({ success: false, message: err });
-                  }
-                  else {
-                    Visitor.findOneAndUpdate({ '_id': visitor._id }, { $set: { password: password }, $push: { "tickets": ticketDoc._id, "exhibitions": ticketDoc.exhibition } }).then(
-                      (err, result) => {
-                        if (err);
-                        Exhibition.findOne({ '_id': metadata.exhibition }, async (err, exhibitionDoc) => {
-                          if (!err) {
-                            {
-                              if (exhibitionDoc) {
-                                try {
-                                  await Exhibition.updateOne({ '_id': metadata.exhibition }, { $push: { "visitors": visitor._id, "tickets": ticketDoc._id } })
-                                }
-                                catch (error) {
-                                  console.log("Error while adding " + visitor._id + " to exhibition( " + metadata.exhibition + " )");
-                                }
-
-                                let transporter = nodemailer.createTransport({
-                                  service: "gmail",
-                                  auth: {
-                                    user: process.env.NODE_MAILER_EMAIL,
-                                    pass: process.env.NODE_MAILER_PASSWORD,
-                                  },
-                                });
-                                try {
-                                  let info = await transporter.sendMail({
-                                    from: '"XPOLAND Team" <xpoland@gmail.com>', // sender address
-                                    to: visitor.email, // list of receivers
-                                    subject: "Coordonnées d'accces à XPOLAND", // Subject line
-                                    html: "<h3>Login : </h3><strong>" + visitor.email + "</strong><br/><h3>Password : </h3><strong>" + password + "</strong><br/><h2 style=\"color:red;\">NB : Veuillez changer votre mot de passe lors de votre première connexion</h2>", // html body
-                                  });
-                                  res.status(201).send({ success: true, message: 'Visitor added successfully.' });
-                                } catch (err) {
-                                  throw ({ success: false, message: "Error while sending e-mail." });
-                                }
-                              }
-                              else {
-                                res.status(404).send({ "error": "Exhibition not found" })
-                              }
-                            }
-                          }
-                          else
-                            res.send(err);
-                        })
+                  } else {
+                    Visitor.findOneAndUpdate(
+                      { _id: visitor._id },
+                      {
+                        $set: { password: password },
+                        $push: {
+                          tickets: ticketDoc._id,
+                          exhibitions: ticketDoc.exhibition,
+                        },
                       }
-                    ).catch(
-                      (error) => {
+                    )
+                      .then((err, result) => {
+                        if (err);
+                        Exhibition.findOne(
+                          { _id: metadata.exhibition },
+                          async (err, exhibitionDoc) => {
+                            if (!err) {
+                              {
+                                if (exhibitionDoc) {
+                                  try {
+                                    await Exhibition.updateOne(
+                                      { _id: metadata.exhibition },
+                                      {
+                                        $push: {
+                                          visitors: visitor._id,
+                                          tickets: ticketDoc._id,
+                                        },
+                                      }
+                                    );
+                                  } catch (error) {
+                                    console.log(
+                                      "Error while adding " +
+                                        visitor._id +
+                                        " to exhibition( " +
+                                        metadata.exhibition +
+                                        " )"
+                                    );
+                                  }
+
+                                  let transporter = nodemailer.createTransport({
+                                    service: "gmail",
+                                    auth: {
+                                      user: process.env.NODE_MAILER_EMAIL,
+                                      pass: process.env.NODE_MAILER_PASSWORD,
+                                    },
+                                  });
+                                  const htmlContent = `
+                                      <h3>Login : </h3><strong>${visitor.email}</strong>
+                                      <br/>
+                                      <h3>Password : </h3><strong>${password}</strong>
+                                      <br/>
+                                      <h2 style="color:red;">NB : Veuillez changer votre mot de passe lors de votre première connexion</h2>
+                                  `;
+                                  await sendEmail(
+                                    visitor.email,
+                                    "Coordonnées d'accces à XPOLAND",
+                                    htmlContent
+                                  );
+                                } else {
+                                  res
+                                    .status(404)
+                                    .send({ error: "Exhibition not found" });
+                                }
+                              }
+                            } else res.send(err);
+                          }
+                        );
+                      })
+                      .catch((error) => {
                         res.status(400).send({
                           success: false,
-                          message: error
+                          message: error,
                         });
-                      }
-                    );
+                      });
                   }
-
-                })
-
-              }
-              else {
+                });
+              } else {
                 var visitor = new Visitor();
                 visitor.email = metadata.email;
                 visitor.password = password;
@@ -125,66 +143,78 @@ stripe = require('stripe')(process.env.STRIPE_SECRET_KEY),
                     ticket.save((err, ticketDoc) => {
                       if (err) {
                         console.log({ success: false, message: err });
-                      }
-                      else {
-                        Exhibition.findOne({ '_id': metadata.exhibition }, async (err, exhibition) => {
-                          if (!err) {
-                            {
-                              if (exhibition) {
-                                if (!exhibition.visitors.includes(visitor._id))
-                                  try {
-                                    await Exhibition.updateOne({ '_id': metadata.exhibition }, { $push: { "visitors": visitor._id, "tickets": ticketDoc._id } })
-                                  }
-                                  catch (error) {
-                                    console.log("Error while adding " + visitor._id + " to exhibition( " + metadata.exhibition + " )");
-                                  }
-
-                                let transporter = nodemailer.createTransport({
-                                  service: "gmail",
-                                  auth: {
-                                    user: process.env.NODE_MAILER_EMAIL,
-                                    pass: process.env.NODE_MAILER_PASSWORD,
-                                  },
-                                });
-                                try {
-                                  let info = await transporter.sendMail({
-                                    from: '"XPOLAND Team" <3DExhibition@gmail.com>', // sender address
-                                    to: visitor.email, // list of receivers
-                                    subject: "Coordonnées d'accces à XPOLAND", // Subject line
-                                    html: "<h3>Login : </h3><strong>" + visitor.email + "</strong><br/><h3>Password : </h3><strong>" + password + "</strong><br/><h2 style=\"color:red;\">NB : Veuillez changer votre mot de passe lors de votre première connexion</h2>", // html body
+                      } else {
+                        Exhibition.findOne(
+                          { _id: metadata.exhibition },
+                          async (err, exhibition) => {
+                            if (!err) {
+                              {
+                                if (exhibition) {
+                                  if (
+                                    !exhibition.visitors.includes(visitor._id)
+                                  )
+                                    try {
+                                      await Exhibition.updateOne(
+                                        { _id: metadata.exhibition },
+                                        {
+                                          $push: {
+                                            visitors: visitor._id,
+                                            tickets: ticketDoc._id,
+                                          },
+                                        }
+                                      );
+                                    } catch (error) {
+                                      console.log(
+                                        "Error while adding " +
+                                          visitor._id +
+                                          " to exhibition( " +
+                                          metadata.exhibition +
+                                          " )"
+                                      );
+                                    }
+                                  const htmlContent = `
+                                    <h3>Login : </h3><strong>${visitor.email}</strong>
+                                    <br/>
+                                    <h3>Password : </h3><strong>${password}</strong>
+                                    <br/>
+                                    <h2 style="color:red;">NB : Veuillez changer votre mot de passe lors de votre première connexion</h2>
+                                `;
+                                  await sendEmail(
+                                    visitor.email,
+                                    "Coordonnées d'accces à XPOLAND",
+                                    htmlContent
+                                  );
+                                  let transporter = nodemailer.createTransport({
+                                    service: "gmail",
+                                    auth: {
+                                      user: process.env.NODE_MAILER_EMAIL,
+                                      pass: process.env.NODE_MAILER_PASSWORD,
+                                    },
+                                  });çi
+                                } else {
+                                  console.log({
+                                    error: "Exhibition not found",
                                   });
-                                  console.log({ success: true, message: 'Visitor added successfully.' });
-                                } catch (err) {
-                                  console.log({ success: false, message: "Error while sending e-mail." });
                                 }
                               }
-                              else {
-                                console.log({ "error": "Exhibition not found" })
-                              }
-                            }
+                            } else console.log(err);
                           }
-                          else
-                            console.log(err);
-                        })
-
+                        );
                       }
-
-                    })
-                  }
-                  else {
-                    console.log({ 'error': err });
+                    });
+                  } else {
+                    console.log({ error: err });
                   }
                 });
               }
+            } else {
+              console.log(err);
             }
-            else { console.log(err); }
-          })
+          });
         }
 
         break;
       default:
     }
     res.sendStatus(200);
-
-  }
-
+  });
